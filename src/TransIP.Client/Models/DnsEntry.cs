3861f3b01b4fafbbe1d1c1e4bbe4d4a18e8d83d2
Mario Lamers <mlamers@assert-development.nl>
2024-02-09 21:38:27 +0100

    Fixed but in naming conversion with jsonOptions policy

--- src/TransIP.Client/Models/DnsEntry.cs
+++ src/TransIP.Client/Models/DnsEntry.cs
@@ -1,10 +1,14 @@
-﻿namespace TransIP.Client.Models
+﻿using System.Text.Json.Serialization;
+using TransIP.Client.Enums;
+
+namespace TransIP.Client.Models
 {
     public class DnsEntry
     {
         public string Name { get; set; }
         public int Expire { get; set; }
-        public string Type { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public DnsEntryType Type { get; set; }
         public string Content { get; set; }
     }
 }
